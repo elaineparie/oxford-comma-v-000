94e257597a794f6a34ae839f003f068f59b870f9
Elaine Anderson <eparie@bu.edu>
2018-05-21 17:43:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length > 2
-  end_of_string = array.splice(1..-1) 
-    array.join(", ") << 
+  end_of_string = array.splice(1..-1)
+    array.join(", ") 
   else
     array.join
   end
