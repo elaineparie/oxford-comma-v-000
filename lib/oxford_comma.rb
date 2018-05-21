e3a0b879b94ab7cd6c891627b17d20f3b79f5382
Elaine Anderson <eparie@bu.edu>
2018-05-21 17:44:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length > 2
   end_of_string = array.slice!(-2..-1)
-    array.join(", ")
+    array.join(", ") << end_of_string
   else
     array.join
   end
