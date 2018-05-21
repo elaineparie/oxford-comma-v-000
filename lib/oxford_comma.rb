0f3ddcb427b4c3cf8c6c22b3f93cddc45c9716ee
Elaine Anderson <eparie@bu.edu>
2018-05-21 17:38:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length > 2
   array.delete_at(-1)
-    array.join(", ")
+    array.join(", ") << array(-1).join
   else
     array.join
   end
